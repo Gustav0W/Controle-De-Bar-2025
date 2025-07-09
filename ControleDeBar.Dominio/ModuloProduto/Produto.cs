1e011959b7fd35fc60205f95dc5f0a97a8dfeb62
GustavoW <guvieirawalter@gmail.com>
2025-07-09 17:03:45 -0300

    Implementado modulo domínio de produto

---
+++ ControleDeBar.Dominio/ModuloProduto/Produto.cs
@@ -0,0 +1,31 @@
+﻿using ControleDeBar.Dominio.Compartilhado;
+
+namespace ControleDeBar.Dominio.ModuloProduto;
+
+public class Produto : EntidadeBase<Produto>
+{
+    public string Nome { get; set; }
+    public decimal Valor { get; set; }
+
+    public Produto() { }
+
+    public Produto(string nome, decimal valor) : this()
+    {
+        Id = Guid.NewGuid();
+        Nome = nome;
+        Valor = valor;
+    }
+
+    public Produto(Guid guid, string nome, decimal valor)
+    {
+        Id = guid;
+        Nome = nome;
+        Valor = valor;
+    }
+
+    public override void AtualizarRegistro(Produto registroEditado)
+    {
+        Nome = registroEditado.Nome;
+        Valor = registroEditado.Valor;
+    }
+}
